fa8ce6efc1c58911785ac8b0e2e3939d95299d45
Tailene Gomes da Silva <182300638@senacinformatica.br>
2024-03-06 21:24:47 -0300

    atualizando

--- caixa.py
+++ caixa.py
@@ -3,6 +3,8 @@ import string
 from tqdm  import tqdm
 import time
 
+
+
 class banco:
  def __init__(self, titular, codigo):
      self.titular: titular
@@ -10,7 +12,7 @@ class banco:
      self.saldo = 0
      self.codigo = codigo
 
-
+  #Realizar depósito#
  def depositar(self, valor):
         self.saldo += valor
         print()
@@ -19,7 +21,9 @@ class banco:
            time.sleep
         print()
         print(f'Depósito realizado com sucesso! Novo saldo: R${self.saldo:.2f}')
+         
 
+      #Realizar saque#
  def sacar(self, valor):
      if valor > self.saldo:
          print()
@@ -37,6 +41,7 @@ class banco:
          print()
          print(f'Saque realizado com sucesso! Novo saldo: R${self.saldo:.2f}')
          
+       #Realizar consulta de saldo#    
  def consultar(self):
          print()
          print('Atualizando saldo...')
@@ -46,7 +51,8 @@ class banco:
          print(f'Titular: {self.titular}')
          print(f'Número da conta: {self.numero_conta}')
          print(f'Saldo: R${self.saldo:.2f}')
-     
+
+        #Realizar tranferencia# 
  def transferir(self, valor, numero_conta):
         if valor > self.saldo:
          print()
@@ -63,6 +69,8 @@ class banco:
              time.sleep(1)
              print()
              print(f'Transferência realizada com sucesso para a conta {conta.numero_conta}!')
+
+        #Criar conta#       
         while True:
             usuario = input('Cadastre seu nome: ')
             if all(c.isalpha() for c in usuario):
@@ -90,8 +98,8 @@ class banco:
         else:
             print(f'Bem vindo(a) {usuario}')
 
-        conta = banco(usuario, senha)
-        conta.numero_conta = random.randint(10000, 99999)
+            conta = banco(usuario, senha)
+            conta.numero_conta = random.randint(10000, 99999)
             print()
             print(f'Sua conta foi criada com sucesso! Seu número de conta é: {conta.numero_conta}.')
             print()
@@ -137,11 +145,27 @@ def login():
                 else:
                     print(f'Bem vindo(a) {cliente}')
                 conta = banco(cliente, codigo)
+                conta.numero_conta = random.randint(10000, 99999)
+                print()
+                print('  Carregando Sistema...')
+                for i in tqdm(range(2)):
+                    time.sleep(1)
+                print()
+                print(f'Sua conta foi criada com sucesso! O número da sua conta é: {conta.numero_conta}.')
+                print()
+                while True:
+                    print('\n ------------  MENU  ------------')
+                    print('1 - Consultar Saldo')
+                    print('2 - Depositar')
+                    print('3 - Sacar')
+                    print('4 - Transferir')
+                    print('5 - Sair')
+                    opcao = input('Digite a opção desejada: ')
+                    
                 
 
                
             
-             
-         
-            
+if __name__ == '__main__':
+    login()
     
